cdb353712b2847700607ec887a54aaa4a911fa42
gillyspace27 <gilly@swri.org>
2024-11-12 13:07:57 -0700

    Initial Commit

---
+++ FITSQuickLookUITests/FITSQuickLookUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  FITSQuickLookUITestsLaunchTests.swift
+//  FITSQuickLookUITests
+//
+//  Created by Gilbert, Gilly on 11/12/24.
+//
+
+import XCTest
+
+final class FITSQuickLookUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
